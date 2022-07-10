5d69be035315e65f580d31c1a741b1fef0a93b4f
Priyanshu Agrawal <priyanshuagrawal419@gmail.com>
2022-07-10 17:05:30 +0530

    pull it

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
-// import Contact from './components/Contact';
-// import Images from './components/Images';
-import OurIncubatesData from './components/OurIncubatesDataExistingIncubates';
-import OurIncubatesTable from './components/OurIncubatesTableExistingIncubates';
+import Contact from './components/Contact';
+// import OurIncubatesData from './components/OurIncubatesDataExistingIncubates';
+// import OurIncubatesTable from './components/OurIncubatesTableExistingIncubates';
 // import OurIncubatesGraduatesIncubatesData from './components/OurIncubatesGraduatesIncubatesData';
 // import OurIncubatesTableGraduated from './components/OurIncubatesGraduatedTable'
 
 function App() {
   return (
   <>
-  <OurIncubatesData />
-  <OurIncubatesTable />
+ <Contact />
   </>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,13 +4,42 @@ export default function Contact() {
   return (
     <>
     <div className='heading'>
-    <h2>Contact Us</h2>
+    <h1>Contact Us</h1>
     </div>
     <div className='content'>
     <div className='map'>
     </div>
     </div>
-    
+    <div className="container">
+                <div className="container1">
+                    <div className="team" id="team1">
+                        <div className="position">
+                            <b>General Secretery</b>
+                        </div>
+                        <div className="name">
+                            <b>Prof.M.K.Sanyal</b><i
+                                className="fas fa-envelope" onClick={() => {
+                                    window.open('mailto:sec.tcgtbi@iiests.ac.in')
+                                }}></i>
+                            <i className="fa-solid fa-phone" onClick={() => {
+                                window.open('tel:944949990')
+                            }}></i>
+                        </div>
+                    </div>
+                    <div className="team" id="team2">
+                        <div className="image">
+                        </div>
+                        <div className="position">
+                            <b>General Manager</b>
+                        </div>
+                        <div className="name">
+                            <b>Dr.Juni Deb Mallick Dutta</b><i
+                                className="fas fa-envelope" onClick={() => { window.open('mailto:manager.tcgtbi@iiests.ac.in') }}></i>
+                            <i className="fa-solid fa-phone" onClick={() => { window.open('tel:09831036650') }}></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
     </>
   )
 }
